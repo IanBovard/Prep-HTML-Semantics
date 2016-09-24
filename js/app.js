34d42e328504e04ac5b123b247029015043122e4
Ian Bovard <thebestpantsintown@gmail.com>
2016-09-23 18:11:55 -1000

    js

---
+++ js/app.js
@@ -0,0 +1,8 @@
+
+    function toggle(id) {
+       var showHide = document.getElementById(id);
+       if(showHide.style.display == "none")
+          showHide.style.display = "block";
+       else
+          showHide.style.display = "none";
+    }
